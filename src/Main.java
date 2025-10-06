deb4be53b4ed0e038e84856b03a018bea38b95c8
dmxxd <denism2k7@gmail.com>
2025-10-06 15:09:02 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,19 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Поток "Яйцо"
-        Thread egg = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
-                try {
-                    Thread.sleep(100); // задержка для наглядности
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-                System.out.println("Яйцо");
-            }
-        });
-        // Запускаем поток "Яйцо"
-        egg.start();
-        // Пока поток "Яйцо" живой, выводим "Курица"
-        while (egg.isAlive()) {
+        // Последовательный (однопоточный) вывод
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Яйцо");
             try {
-                Thread.sleep(100); // чтобы чередовались
+                Thread.sleep(100); // просто задержка для наглядности
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
             System.out.println("Курица");
         }
-        // Дожидаемся завершения потока "Яйцо"
-        try {
-            egg.join();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
+
         // Итог спора
         System.out.println("Спор закончен!");
-        if (egg.isAlive()) {
-            System.out.println("Победило яйцо!");
-        } else {
-            System.out.println("Победила курица!");
-        }
+        System.out.println("Победила курица!");
     }
 }
-
-
